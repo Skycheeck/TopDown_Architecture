086b7a2e2c8c5705e27db17b580e71b43370570a
Skycheeck <Sergeyxow@yandex.ru>
2024-05-22 15:24:41 +0300

    Queue of destination points

--- Assets/Scripts/MoveToClick.cs
+++ Assets/Scripts/MoveToClick.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MoveToClickPoint : MonoBehaviour
 {
     private NavMeshAgent _agent;
+    private readonly Queue<Vector3> _destinationPoints = new();
+    
     private const string WALKABLE_AREA = "Walkable Area";
 
     private void Start()
@@ -13,10 +16,35 @@ public class MoveToClickPoint : MonoBehaviour
 
     private void Update()
     {
+        CheckForInput();
+        
+        // no destination points
+        if (_destinationPoints.Count < 1) return;
+        
+        // no path yet
+        if (!_agent.hasPath)
+        {
+            SetDestinationPoint();
+            return;
+        }
+        
+        // distance is too long
+        if (_agent.remainingDistance > _agent.stoppingDistance) return;
+        
+        SetDestinationPoint();
+    }
+
+    private void SetDestinationPoint()
+    {
+        _agent.destination = _destinationPoints.Dequeue();
+    }
+
+    private void CheckForInput()
+    {
         if (!Input.GetMouseButtonDown(0)) return;
         if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100)) return;
         if (!hit.collider.gameObject.CompareTag(WALKABLE_AREA)) return;
 
-        _agent.destination = hit.point;
+        _destinationPoints.Enqueue(hit.point);
     }
 }
