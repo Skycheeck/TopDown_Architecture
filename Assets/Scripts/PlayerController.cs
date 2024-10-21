83715544a401169540d31176df057769026cb96d
Skycheeck <Sergeyxow@yandex.ru>
2024-10-21 16:41:56 +0300

    PlayerController fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,18 @@ using VContainer.Unity;
 public class PlayerController : ITickable
 {
     private readonly CharacterController _characterController;
+    private readonly EventSystem _eventSystem;
     private const string WALKABLE_AREA = "Walkable Area";
 
-    public PlayerController(CharacterController characterController) => _characterController = characterController;
+    public PlayerController(CharacterController characterController, EventSystem eventSystem)
+    {
+        _eventSystem = eventSystem;
+        _characterController = characterController;
+    }
 
     public void Tick()
     {
-        if (!Input.GetMouseButtonDown(0) || EventSystem.current.IsPointerOverGameObject()) return;
+        if (!Input.GetMouseButtonDown(0) || _eventSystem.IsPointerOverGameObject()) return;
         if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100)) return;
         if (!hit.collider.gameObject.CompareTag(WALKABLE_AREA)) return;
 
